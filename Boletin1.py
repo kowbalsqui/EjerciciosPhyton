213a243d772a51eb682fd3668e07294581007187
Carlos Perea <carlos.perea.figuera.al@iespoligonosur.org>
2024-09-24 10:17:31 +0200

    Apuntes y Ejercicios 24 Septiembre

---
+++ Boletin1.py
@@ -0,0 +1,49 @@
+#Ejercicio 1:
+
+print("Hola mundo")
+
+#Ejercicio 2:
+
+numero1 = input("Ingresa el primer número:")
+numero2 = input("Ingresa el segundo número:")
+
+suma = int(numero1) + int(numero2)
+print("El resultado de la suma es: " + str(suma))
+
+#Ejercicio 3: 
+
+base = input("Ingresa la base del triangulo: ")
+altura = input("Ingresa la altura del triangulo: ")
+
+area = int(base) * int(altura) / 2
+print ("El área del triangulo es: " + str(area))
+
+#Ejercicio 4:
+
+celsius = input("Ingresa los grados Celsius que pasaran a fahrenheit: ")
+
+faren = int(celsius) * 9 / 5 + 32
+print("Los grados Celsius pasados a Fahrenheit son: " + str(faren))
+
+#Ejercicio 5:
+
+numF = input("Ingrese el numero factorial ")
+numero3 = 1
+
+for i in range (1, int(numF) + 1):
+    numero3 = numero3 * i
+print("El factorial de " + numF + " es: " + str(numero3))
+
+#Ejercicio 6:
+
+numero4 = input("Ingrese el numero ")
+if (int(numero4) % 2 == 0):
+    print("El numero es par")
+else:
+    print("El numero es impar")
+    
+#Ejercicio 7:
+
+numero5 = input("Ingresa un número ")
+numero6 = input("Ingresa otro número ")
+
---
+++ EjerciciosPhytonRepaso.py
@@ -0,0 +1,28 @@
+#prueba = "Hola mundo"
+#print(prueba)
+
+#edad = 25
+#if edad >= 18:
+#    print("Eres mayor de edad")
+#    print("Puedes votar")
+#if edad >= 30:
+#    print("Tienes derecho a un bebé")
+#else :
+#    print("No puedes votar")
+#print ("Hola")
+
+#año = 2000
+#while año <= 2022:
+#    print("Este año es " + año)
+#    año += 1
+#print("Fin del ciclo")
+
+#for i in range(0,10):
+#    for j in range (0,10):
+#        print("position " +str(i)+ "," +str(j))
+#print("Fin del bucle")
+
+#lista = [1, 2, 3, 4, 5]
+#for x in lista:
+#    print("Elemento de la lista: " + str(x))
+#print("Hemos terminado")
